ea52b1dc1616ac7238bd6e2bbc47b9f4660b9b09
madlife <2358460379@qq.com>
2024-01-13 21:18:54 +0800

    调试相关代码

---
+++ Day14/Day14/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include<stdlib.h>
+//int main() {
+//	int arr[10] = { 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	int i = 0;
+//	for (i = 0; i < sz; i++) {
+//		arr[i] = i + 1;
+//	}
+//	//若确定16行之前的代码没有问题，而问题出现在16行之后，则可以直接将断点打在16行，
+//	//调试直接一步运行前15行代码(省去前面的循环语句节省时间)，再从16行逐句调试，节省时间
+//	for (i = 0; i < sz; i++) {
+//		printf("%d\n", arr[i]);
+//	}
+//	return 0;
+//}
+
+//void test2() {
+//	printf("hhh");
+//}
+//void test1() {
+//	test2();
+//}
+//void test() {
+//	test1();
+//}
+//int main() {
+//	test();
+//	return 0;
+//}
---
+++ Day14/Day14/test2.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include<stdlib.h>
+int main() {
+	int i;
+	int arr[16];
+	
+	for (i = 0; i <=18; i++)
+	{
+		arr[i] = 0;
+		printf("%d",i);
+
+	}
+	return 0;
+}
+
+//int main() {
+//	char arr[] = "xxx";
+//	char arr1[] = "hello";
+//	strcpy(arr, arr1);
+//	printf("%s", arr);
+//}
